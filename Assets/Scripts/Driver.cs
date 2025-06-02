88d70891e7d6b3c32a7d9f0012cc87dbeedddcc6
PaladinWolf2675 <timberwolf2675@gmail.com>
2025-06-02 03:28:49 -0400

    Re updated variables for steering and movement speed. Added serialized field attribute to both variables.

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField]
+    float steerSpeed = 0.1f;
+    [SerializeField]
+    float moveSpeed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        transform.Rotate(0, 0, 45);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
